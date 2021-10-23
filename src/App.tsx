a5fb011724e7d66778d1a22c7e4560a90e2073fa
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-10-23 23:18:20 +0300

    Try saga structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
+
 const App = () => {
+  const store = useSelector(store => store);
+  console.log(store);
   return (
     <div>
       React inreractive map
