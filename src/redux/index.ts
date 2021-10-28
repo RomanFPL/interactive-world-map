71c83fd0875c2b90a6006dfd0a4053fca341e2b3
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-10-28 04:37:29 +0300

    Get data with any type with saga

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,13 +2,13 @@ import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import reducer from './reducers'
 
-import { helloSaga } from './sagas'
+import { getApiData, helloSaga } from './sagas'
 
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
   reducer,
   applyMiddleware(sagaMiddleware)
 )
-sagaMiddleware.run(helloSaga)
+sagaMiddleware.run(getApiData)
 
 export {store}
--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,3 +1,35 @@
-export function* helloSaga() {
-    console.log('Hello Sagas!')
+import axios from "axios"
+
+class ApiService {
+  _url = "http://jsonplaceholder.typicode.com/posts"
+
+  getData = async() => {
+    try{
+      const temp : any = await axios.get("http://jsonplaceholder.typicode.com/posts");
+      return temp.data
+    } catch(e) {
+      console.dir(e);
+    }
   }
+
+}
+
+interface ResponseGenerator{
+  config?:any,
+  data?:any,
+  headers?:any,
+  request?:any,
+  status?:number,
+  statusText?:string
+}
+
+const apiService = new ApiService();
+
+export function* helloSaga() {
+    yield console.log('Hello Sagas!')
+  }
+
+export function* getApiData(){
+  const temp:ResponseGenerator | void = yield axios.get("http://jsonplaceholder.typicode.com/posts");
+  yield console.log(temp);
+}
