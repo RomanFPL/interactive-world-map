3445071399564bfaf3e35041af909c8eedbb09c5
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-10-29 23:44:02 +0300

    Saga for WB get all countries.

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,11 +1,32 @@
 import axios from "axios"
 
-class ApiService {
-  _url = "http://jsonplaceholder.typicode.com/posts"
+interface ResponseGenerator{
+  config?:any,
+  data?: Array<any>,
+  headers?:any,
+  request?:any,
+  status?:number,
+  statusText?:string
+}
+
+
+class WorldBanckApiService {
+  _url = "http://api.worldbank.org/v2/";
+  _format = "?format=json";
+
+  getAllCountries = async () => {
+    try{
+      const countruList = await axios.get(this._url + "country"+this._format+"&per_page=300");
+      return countruList.data;
+    }catch(e){
+      console.log("It is impossible to get all countries from api!");
+    }
+  }
+
 
   getData = async() => {
     try{
-      const temp : any = await axios.get("http://jsonplaceholder.typicode.com/posts");
+      const temp : any = await axios.get(this._url);
       return temp.data
     } catch(e) {
       console.dir(e);
@@ -14,22 +35,13 @@ class ApiService {
 
 }
 
-interface ResponseGenerator{
-  config?:any,
-  data?:any,
-  headers?:any,
-  request?:any,
-  status?:number,
-  statusText?:string
-}
-
-const apiService = new ApiService();
+const apiWB = new WorldBanckApiService();
 
 export function* helloSaga() {
     yield console.log('Hello Sagas!')
   }
 
 export function* getApiData(){
-  const temp:ResponseGenerator | void = yield axios.get("http://jsonplaceholder.typicode.com/posts");
+  const temp:ResponseGenerator | void = yield apiWB.getAllCountries();
   yield console.log(temp);
 }
